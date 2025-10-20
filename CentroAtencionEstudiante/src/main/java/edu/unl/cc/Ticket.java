68e05c67f94161f649a8d12fa80b148ac926d2bd
Darwin090 <darwin.correa@unl.edu.ec>
2025-10-19 22:08:17 -0500

    Reformat Ticket class with consistent spacing

--- CentroAtencionEstudiante/src/main/java/edu/unl/cc/Ticket.java
+++ CentroAtencionEstudiante/src/main/java/edu/unl/cc/Ticket.java
@@ -6,7 +6,7 @@ public class Ticket {
     String tipoTramite;
     EstadoTicket estado;
     ListaNotas notas;
-
+//Creamos un constructor y un metodo para que muestre la informacion
     public Ticket(int id, String estudiante, String tipoTramite) {
         this.id = id;
         this.estudiante = estudiante;
@@ -15,10 +15,11 @@ public class Ticket {
         this.notas = new ListaNotas();
 
     }
-//Creamos un constructor y un metodo para que muestre la informacion
+
     public void mostrarInfo() {
         System.out.println("Ticket #" + id + "|" + estudiante + "|" + tipoTramite + "| Estado: " + estado);
     }
 
 }
+
 
